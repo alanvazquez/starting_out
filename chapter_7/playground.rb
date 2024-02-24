c17abaa6dba1cf193d389d79905e52f5b8d49b3a
Alan Vázquez <5857174+alanvazquez@users.noreply.github.com>
2024-02-24 12:35:41 -0600

    added new exercises

--- chapter_7/playground.rb
+++ chapter_7/playground.rb
@@ -1,39 +1,30 @@
-def ask_for_fat
-  puts "Enter the number of fat grams: "
-  gets.chomp.to_f
+puts "A".ord
+
+puts "alan".bytes
+
+puts "a".ord + 2
+
+puts 97.chr
+
+puts "h".ord
+
+def change_letters(letter)
+  new_letter = letter.ord + 2
+  puts "#{new_letter.chr}"
 end
 
-def ask_for_calories
-  puts "Enter the number of calories:"
-  gets.chomp.to_i
+def change_string(string)
+  new_string = string.bytes
+  new_string.each { |letter| letter.ord + 2}
+  puts new_string
 end
 
-def validate_input(input)
-  while input < 0
-    puts "Input cannot be less than 0. Please enter a valid number:"
-    input = gets.chomp.to_f
-  end
-  input
-end
+puts "enter a letter"
+letter = gets.chomp
 
-def calculate_fat_percentage(fat_grams, calories)
-  percentage = (fat_grams * 9) / calories
-  if percentage < 0.3
-    puts "This food is low in fat."
-  else
-    puts "This food is not low in fat."
-  end
-end
+change_letters(letter)
 
-# Program Starts
 
-fat_grams = ask_for_fat
-fat_grams = validate_input(fat_grams)
-calories = ask_for_calories
-calories = validate_input(calories)
-while calories > fat_grams * 9
-  puts "Calories cannot exceed nine times the fat grams. Please enter a valid number of calories:"
-  calories = gets.chomp.to_i
-end
-
-calculate_fat_percentage(fat_grams, calories)
+puts "enter a string"
+string = gets.chomp
+change_string(string)
