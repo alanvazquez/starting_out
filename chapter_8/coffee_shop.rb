c20d5f203b65e4f545e985e02d0208b669bf5262
Alan Vázquez <5857174+alanvazquez@users.noreply.github.com>
2024-02-28 10:36:38 -0600

    updated folders

---
+++ chapter_8/coffee_shop.rb
@@ -0,0 +1,13 @@
+def ask_hourly_rate
+  puts "Enter the hourly rate"
+  gets.chomp.to_f
+end
+
+def ask_worked_hours
+  puts "Enter worked hours"
+  employees= []
+  employees.each do |employee|
+  puts "Hi " + employee
+end
+  gets.chomp.to_f
+end
